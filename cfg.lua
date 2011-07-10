5fb28566804e926da227b7499a56a478671f2d9e
unknown <nopstar@.(none)>
2011-07-10 17:49:08 +0200

    Config of Sniffles ACP

---
+++ cfg.lua
@@ -0,0 +1,22 @@
+-- // Short Config
+-- // 1 - 12 Buttons are possible!
+-- // Want more Buttons? Contact me on Tukui.org
+
+sn["ACP"] = {
+	["RCBtn"] = 1, 				-- // Ready Check Button
+	["LeavePBtn"] = 2, 			-- // Leave Party Button
+	["FocusBtn"] = 3,			-- // Set Focus Button
+	["MoveUIBtn"] = 4,			-- // Move UI Button
+	["UIHelpBtn"] = 5,			-- // UI Help Button
+	["CvtPtR"] = 6,				-- // Convert Party to Raid Button
+	["CvtRtP"] = 7,				-- // Convert Raid to Party Button
+	["CfgBtn"] = 8,				-- // GUI Call Button
+	["DPSLayoutBtn"] = 9,		-- // DPS Layout Button
+	["HealLayoutBtn"] = 10,		-- // Heal Layout Button
+	["ReloadUIBtn"] = 0,			-- // Reload UI Button
+	["LoadAddOnBtn"] = 0,		-- // Disable AddOn Button
+	["DisableAddOnBtn"] = 11,		-- // Enable AddOn Button
+	["HoverBindBtn"] = 0,		-- // Set Hoverbind Button
+	["CombatLogFixBtn"] = 0,		-- // Fix CombatLog Button
+	["AutoInviteBtn"] = 12,		-- // AutoInvite On/Off Button
+}
